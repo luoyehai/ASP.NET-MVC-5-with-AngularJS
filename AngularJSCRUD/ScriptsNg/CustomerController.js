294ec4b33742d3e02605364cff942b73bbf92045
luoyehai <zlhluoye@163.com>
2018-03-05 09:58:58 +0800

    Update
    update

--- AngularJSCRUD/ScriptsNg/CustomerController.js
+++ AngularJSCRUD/ScriptsNg/CustomerController.js
@@ -1,9 +1,10 @@
-﻿angular.module('myFormApp', [])
-    .controller('CustomerController', function ($scope, $http, $location, $window) {
+﻿var app = angular.module('myFormApp', []);
+app.controller('CustomerController', function ($scope, $http, $location, $window) {
         $scope.Rating = {};
         $scope.Rating.EmployeeName = $("#employeename").val();
         $scope.Rating.Overall_Rating = 0;
         $scope.Rating.CreateOn = new Date();
+        $scope.checkedmodule = [];
         GetLogin();
         $scope.Rating.BasicOperselected = null;
         $scope.Rating.Priorityselected = null;
@@ -15,6 +16,7 @@
                 { title: 'FI', checked: false },
                 { title: 'CO', checked: false },
                 { title: 'MM', checked: false },
+                { title: 'PP', checked: false },
                 { title: 'SD', checked: false },
                 { title: 'WM', checked: false },
                 { title: 'PM', checked: false },
@@ -65,37 +67,74 @@
             { title: 'Good', checked: false, value: 4 },
             { title: 'Excellent', checked: false, value: 5 }
         ];
-        $scope.updateSAPModuleSelection = function (position, SAPModuleS, title) {
-            $scope.Rating.SAP_Module = $scope.GetCheckedValue(position, SAPModuleS, title);
+        $scope.updateSAPModuleSelection = function (position, SAPModule, SAPModuleS, title) {
+            var index = $scope.checkedmodule.indexOf(SAPModule.title);
+            if (index > -1) {
+                $scope.checkedmodule.splice(index, 1);
+            }
+            else {
+                $scope.checkedmodule.push(SAPModule.title.trim());
+            }
+            //$scope.Rating.SAP_Module = $scope.GetCheckedValue(position, SAPModuleS, title);
         }   
         //basic Operation
         $scope.updateBasicOperSelection = function (position, RatingBasic, RatingsBasic, value) {
-            $scope.Rating.Basic_Operation = $scope.GetCheckedRatingValue(position, RatingBasic, RatingsBasic, value);
+            if (RatingBasic.checked) {
+                $scope.Rating.Basic_Operation = $scope.GetCheckedRatingValue(position, RatingBasic, RatingsBasic, value);
+            }
+            else {
+                $scope.Rating.Basic_Operation = 0;
+            }
             $scope.changeratingnumber();
         }
         //Priority in your daily work
         $scope.updatePrioritySelection = function (position, RatingPriority, RatingsPriority, value) {
-            $scope.Rating.Priority_Daily_Work = $scope.GetCheckedRatingValue(position, RatingPriority, RatingsPriority, value);
+            if (RatingPriority.checked) {
+                $scope.Rating.Priority_Daily_Work = $scope.GetCheckedRatingValue(position, RatingPriority, RatingsPriority, value);
+            }
+            else {
+                $scope.Rating.Priority_Daily_Work = 0;
+            }
             $scope.changeratingnumber();
         }
         //Document Resource
         $scope.updateDocumentSelection = function (position, RatingDocument, RatingsDocument, value) {
-            $scope.Rating.Document_Resource = $scope.GetCheckedRatingValue(position, RatingDocument, RatingsDocument, value);
+            if (RatingDocument.checked) {
+                $scope.Rating.Document_Resource = $scope.GetCheckedRatingValue(position, RatingDocument, RatingsDocument, value);
+            }
+            else {
+                $scope.Rating.Document_Resource = 0;
+            }
             $scope.changeratingnumber();
         }
         //Company SAP Learning Environment
         $scope.updateCompanySAPSelection = function (position, RatingCompany, RatingsCompany, value) {
-            $scope.Rating.SAP_Environment = $scope.GetCheckedRatingValue(position, RatingCompany, RatingsCompany, value);
+            if (RatingCompany.checked) {
+                $scope.Rating.SAP_Environment = $scope.GetCheckedRatingValue(position, RatingCompany, RatingsCompany, value);
+            }
+            else {
+                $scope.Rating.SAP_Environment = 0;
+            }
             $scope.changeratingnumber();
         }
         //Same Dept Colleagues SAP ERP skill
         $scope.updateDeptColleaguesSelection = function (position, RatingDept, RatingsDept, value) {
-            $scope.Rating.Dept_Colleagues_SAP_ERP = $scope.GetCheckedRatingValue(position, RatingDept, RatingsDept, value);
+            if (RatingDept.checked) {
+                $scope.Rating.Dept_Colleagues_SAP_ERP = $scope.GetCheckedRatingValue(position, RatingDept, RatingsDept, value);
+            }
+            else {
+                $scope.Rating.Dept_Colleagues_SAP_ERP = 0;
+            }
             $scope.changeratingnumber();
         }
         //Line manager SAP ERP skill
         $scope.updateManagerSAPSelection = function (position, RatingLine, RatingsLine, value) {
-            $scope.Rating.Line_Manager_SAP_ERP = $scope.GetCheckedRatingValue(position, RatingLine, RatingsLine, value);
+            if (RatingLine.checked) {
+                $scope.Rating.Line_Manager_SAP_ERP = $scope.GetCheckedRatingValue(position, RatingLine, RatingsLine, value);
+            }
+            else {
+                $scope.Rating.Line_Manager_SAP_ERP = 0;
+            }
             $scope.changeratingnumber();
         }
         //rating number
@@ -130,19 +169,20 @@
             return number;
         }
         $scope.GetCheckedValue = function (position, SAPModuleS, title) {
-            angular.forEach(SAPModuleS,
-                function (SAPModule, index) {
-                    if (position != index)
-                        SAPModule.checked = false;
-                    result = title;
-                });
-            return result;
+            //angular.forEach(SAPModuleS,
+            //    function (SAPModule, index) {
+            //        if (position != index)
+            //            SAPModule.checked = false;
+            //    });
+            //$scope.checkedmodule.push(title.trim());
+            //return $scope.checkedmodule;
         }
         $scope.GetCheckedRatingValue = function (position, parrating, parRatings, value) {
             angular.forEach(parRatings,
                 function (parrating, index) {
-                    if (position != index)
+                    if (position != index) {
                         parrating.checked = false;
+                        }
                     result = value;
                 });
             return result;
@@ -159,7 +199,7 @@
             var keepgoing = true;
             //Check check box
             if (keepgoing) {
-                keepgoing = $scope.checkcheckbox($scope.Rating.SAP_Module, "SAP Module");
+                keepgoing = $scope.Checklatestcheckvalue($scope.checkedmodule, "SAP Module");
             }
             if (keepgoing) {
                 keepgoing = $scope.checkcheckbox($scope.Rating.Basic_Operation, "Basic Operation");
@@ -224,8 +264,19 @@
             }
            return true;
         }
+        $scope.Checklatestcheckvalue = function (checkvalue, questiondetail) {
+            if (typeof checkvalue !== 'undefined' && checkvalue.length > 0) {
+                $scope.Rating.SAP_Module = checkvalue.join(",");
+                return true;
+            }
+            else {
+                $window.alert("Please check the  " + questiondetail);
+                return false;
+            }
+
+        }
         $scope.checkcheckbox = function (checkvalue, questiondetail) {
-            if (checkvalue == null ) {
+            if (checkvalue == null || checkvalue == 'undefined') {
                 $window.alert("Please check the  " + questiondetail);
                 return false;
             }
@@ -233,7 +284,7 @@
         }
 
         function GetLogin() {
-            var oldurl = 'test/Home/getLogin';
+            var oldurl = '/Home/getLogin';
             //var url = '@Url.Action("getLogin", "Home")'
             $http.get(oldurl)
                 .success(function (data, status, headers, config) {
@@ -249,12 +300,20 @@
         $scope.UpdateSurveyinDB = function () {
             $http({
                 method: 'POST',
-                url: 'test/Home/SaveRating',
+                url: '/Home/SaveRating',
                 data: $scope.Rating
             }).success(function (data, status, headers, config) {
                 if (data.success === true) {
                     $window.alert("Successfully submitted! Thanks");
                     $scope.Rating = {};
+                    SetCheckboxfalse($scope.SAPModuleS);
+                    SetCheckboxfalse($scope.RatingsBasic);
+                    SetCheckboxfalse($scope.RatingsPriority);
+                    SetCheckboxfalse($scope.RatingsDocument);
+                    SetCheckboxfalse($scope.RatingsCompany);
+                    SetCheckboxfalse($scope.RatingsDept);
+                    SetCheckboxfalse($scope.RatingsLine);
+                    $window.location.reload();
                 }
                 else {
                     $scope.message = 'Unexpected Error while updating the rating!';
@@ -265,6 +324,14 @@
                 $scope.result = "color-red";
                 console.log($scope.message);
             });
+        }
+
+
+        function SetCheckboxfalse(checkgroups){
+            angular.forEach(checkgroups,
+              function (check, index) {
+                  check.checked = false;
+              });
         }
 
     }).config(function ($locationProvider) {
---
+++ AngularJSCRUD/ScriptsNg/ReportController.js
@@ -0,0 +1,33 @@
+﻿var app = angular.module('myFormApp', []);
+app.controller('ReportController', function ($scope, $http, $location, $window) {
+    $scope.basic = 90;
+    $scope.priority = 60;
+    $scope.document = 20;
+    $scope.company = 30;
+    $scope.dept = 40;
+    $scope.line = 50;
+    $scope.depts = [{ dept: 'MM', basic: 3, priority: 1, document: 1, company: 2, colleague: 1, line: 4 }
+                   , { dept: 'PM', basic: 2, priority: 2, document: 4, company: 2, colleague: 2, line: 3 }
+                   , { dept: 'TM', basic: 4, priority: 2, document: 5, company: 2, colleague: 2, line: 8 }
+                     , { dept: 'DOS', basic: 3, priority: 2, document: 3, company: 2, colleague: 1, line: 4 }];
+    $scope.exporttoexcel = function () {
+        $http({
+            method: 'POST',
+            url: 'Home/exporttoResult',
+            data: $scope.Rating
+        }).success(function (data, status, headers, config) {
+            if (data.success === true) {
+                $window.alert("Successfully export!");
+            }
+            else {
+                $scope.message = 'Unexpected Error while doing the export!';
+                console.log($scope.message);
+            }
+        }).error(function (data, status, headers, config) {
+            $scope.message = 'Unexpected Error while doing export!' + data.errors;
+            $scope.result = "color-red";
+            console.log($scope.message);
+        });
+    }
+
+})
